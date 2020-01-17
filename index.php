ef2c190b7f43562eae8bf8e907a519cc44f37f67
NullC0d3rx <58953519+NullC0d3rx@users.noreply.github.com>
2020-01-17 03:26:39 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<html><head>
+<link rel="shortcut icon" href="http://www.clipartbest.com/cliparts/niE/kRL/niEkRL6iA.png">
+
+    <title>Stamped by NullC0d3r</title>
+    <style>
+        body {
+            color: #fff;
+            background: #000;
+            text-align: center;
+            font-family: Verdana;
+            font-size: 19px;
+        }
+        h1 {
+            margin: 3px;
+            font-size: 45px;
+        }
+    </style>
+</head>
+<body><br>
+<h1>Stamped</h1>
+<small>by NullC0d3r 
+ <br><br>
+<img border="0" src="https://i.imgur.com/26I05og.gif" width="550" height="280"><br><br>
+<center>NEITHER WAR BETWEEN HACKERS, NOR PEACE FOR THE SYSTEM.</center><br>
+<br>
+ Gr33tz: Zambrius - vxlKz - Kuroi'SH - KazuyaWebMA - SANNINJA - B4l0x - 1nocent
+ - Aj4x - Hipnotic - Nasty - MRC - Spy_Unkn0wn - Helsinki - L0st3r - Prosox - Protowave - CyberTeam
+<br><br>
+<a href="https://twitter.com/Nullc0d3r" class="twitter-follow-button" data-show-count="true" >Follow @NullC0d3r</a> 
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+<br>
+<iframe width="0" height="0" src="https://www.youtube.com/embed/Wnwi7kZmt_M?autoplay=true" frameborder="0"></iframe>
+<style type="text/css">body, a:hover {cursor: url(http://cur.cursors-4u.net/symbols/sym-1/sym46.cur), progress !important;}</style>
+  </small></body></html>
